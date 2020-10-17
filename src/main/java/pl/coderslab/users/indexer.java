281e1dc85faeda9a4c4c1de132e27fc92258ff77
R0dr1g3z <r0dr1g3zek@gmail.com>
2020-10-17 15:03:04 +0200

    Poprawki

---
+++ src/main/java/pl/coderslab/users/indexer.java
@@ -0,0 +1,20 @@
+package pl.coderslab.users;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/indexer")
+public class indexer extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("http://localhost:8080/").forward(request,response);
+        response.sendRedirect("/user/list");
+    }
+}
