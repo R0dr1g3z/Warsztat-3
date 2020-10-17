230333e891608dc48a1a7c7aa7b9b2650fe0ac52
R0dr1g3z <r0dr1g3zek@gmail.com>
2020-10-17 14:39:42 +0200

    Poprawka na heroku

--- src/main/java/pl/coderslab/users/UserDelete.java
+++ src/main/java/pl/coderslab/users/UserDelete.java
@@ -1,5 +1,8 @@
 package pl.coderslab.users;
 
+import pl.coderslab.utils.User;
+import pl.coderslab.utils.UserDAO;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,6 +17,8 @@ public class UserDelete extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        UserDAO userDAO = new UserDAO();
+        userDAO.delete(Integer.parseInt(request.getParameter("id")));
+        response.sendRedirect("/user/list");
     }
 }
